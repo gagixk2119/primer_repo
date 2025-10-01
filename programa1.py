fce4ee5e0c0bb2e2ef0c57458e15910dd28ca685
gagixk2119 <a385898@uach.mx>
2025-10-01 09:17:56 -0600

    Create programa1.py

---
+++ programa1.py
@@ -0,0 +1,6 @@
+# Este es un script de python de prueba para el repositorio
+def saludo(nombre):
+  return "Hola "+nombre
+
+# Se invoca a la función saludo para saludar a alguien llamado Hugo
+saludo("Hugo")
